739811d66b280c08893c88ae1dd52193e768a675
AlexQuinoMiranda <alexquinom24@gmail.com>
2025-05-04 23:31:57 -0400

    Modo Oscuro

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,1 +1,7 @@
 console.log("CI/CD funcionando correctamente");
+
+const toggle = document.getElementById('darkModeToggle');
+
+toggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+});
